c17ca3ce8c977c692ab51cbae6619b6be137bbca
akrherz <akrherz@iastate.edu>
2013-08-15 11:54:02 -0500

    only save LW

--- htdocs/index.php
+++ htdocs/index.php
@@ -21,16 +21,16 @@
  <ul>
  <li><a href="/data/hrrr_reflectivity/">hrrr_reflectivity</a> - 15 minute
  "composite" and 1km AGL radar reflectivity product.</li>
- <li><a href="/data/hrrr_radiation/">hrrr_radiation</a> - Short wave down at
- the surface and long wave up at the top of the atmosphere.</li>
+ <li><a href="/data/hrrr_radiation/">hrrr_radiation</a> - Long wave up at the 
+ top of the atmosphere.</li>
  </ul>
  
   <H4>Changes</H4>
   <ul>
-   <li><strong>15 Aug 2013</strong>: Add variable subset of shortwave and 
-   longwave radiation.  Your system may not understand these two variables as
+   <li><strong>15 Aug 2013</strong>: Add variable subset of  
+   longwave radiation.  Your system may not understand this variable as
    their grib table usage appears to be very new?  Anyway, look for 193 for 
-   long wave and 192 for short wave within the grib variable parameters.</li>
+   long wave.</li>
   <li><strong>12 Jul 2013</strong>: Added symlink in the hrrr_reflectivity folder
    pointing to the latest reflectivity file.  This link updates once per hour.</li>
   </ul>
